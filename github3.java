3c96938e4e4aa0af45e1199b07ab9c15e500adf2
Shreyashyadav0 <100284030+Shreyashyadav0@users.noreply.github.com>
2022-03-04 10:00:30 +0530

    fibonacci series

--- github3.java
+++ github3.java
@@ -1,6 +1,6 @@
 import java.util.*;
- public class low to high all prime numbers {
-     public staic void main(String[]args){
+ public class github3 {
+     public static void main(String[]args){
          Scanner scn= new Scanner(System.in);
          int n= scn.nextInt();
          int a =0;
